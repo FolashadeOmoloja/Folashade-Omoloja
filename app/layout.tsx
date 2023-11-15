9ca89c1ca13554e801e1df9aa722b979d7bba76e
FolashadeOmoloja <omolojashade@gmail.com>
2023-11-15 23:46:48 +0100

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter, Pacifico } from 'next/font/google'
+import { Inter, Pacifico, Poppins, Raleway } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 
