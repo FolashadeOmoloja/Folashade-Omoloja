8a954e7cc90c0a9c73b5acb1319b5205c3b1874a
FolashadeOmoloja <omolojashade@gmail.com>
2023-11-16 19:49:09 +0100

    sidebar

--- components/Main.tsx/MainBody.tsx
+++ components/Main.tsx/MainBody.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import SideBar from './SideNav/SideBar'
 
 const MainBody = () => {
   return (
     <main className='grid grid-cols-12 gap-5'>
-       <div className='border-orange-600  border  col-span-4 h-[800px]'></div>
-       <div className='border-orange-600  border  col-span-8 h-[800px]' ></div>
+       <div className='border-orange-600  border  col-span-4 h-[470px]'>
+          <SideBar/>
+       </div>
+       <div className='border-orange-600  border  col-span-8 h-[470px]' ></div>
     </main>
   )
 }
--- components/Main.tsx/SideNav/SideBar.tsx
+++ components/Main.tsx/SideNav/SideBar.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Image from 'next/image'
 
 const SideBar = () => {
   return (
-    <div>SideNav</div>
+    <section>
+        <div className='w-[150px] h-[150px] flex justify-center items-center mx-auto  rounded-[20px] overflow-hidden bg-[#FF9C1A]'>
+        <Image
+        src="/shades.png"
+        alt="avatar"
+        height={150}
+        width={150}
+        layout="intrinsic"
+        quality="100"
+      />
+        </div>
+    </section>
   )
 }
 
