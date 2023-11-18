9ad6501a3833720990adb496ba973b8a08d6299c
FolashadeOmoloja <omolojashade@gmail.com>
2023-11-18 21:08:46 +0100

    portfolioDetails-about

--- components/Main.tsx/PortFolioDetails/About.tsx
+++ components/Main.tsx/PortFolioDetails/About.tsx
@@ -14,6 +14,9 @@ const About:React.FC<Iprop> = ({fontFamily}) => {
             <div className='bg-btngradient w-[250px] h-[2px] rounded-[20px]'></div>
         </div>
         <p className='font-[500] leading-[30px]'>Hello there! I'm Folashade, a Front end developer, solving problems one code at a time. I specialize in building Frontend applications, I take pride in my work and ensure I deliver the most accessible and most responsive application possible. It's not just about writing code; it's about building experiences that leave a lasting impression <img src="emoji.png" alt=""  className='w-5 h-5 inline-block'/>.</p>
+        <section className='mt-[26px]'>
+              <h3 className={`${fontFamily} font-semibold text-[32px] leading-[40px] tracking-[2.5%] mb-6`}>What I do </h3>
+        </section>
         
    </section>
   )
