a7df216b925d9d9541c3f119624bc35be81ec682
FolashadeOmoloja <omolojashade@gmail.com>
2023-11-21 21:54:43 +0100

    portfolioDetails-contact

--- components/Main.tsx/PortFolioDetails/Contact.tsx
+++ components/Main.tsx/PortFolioDetails/Contact.tsx
@@ -1,3 +1,10 @@
+'use client'
+
+import { useState } from "react";
+import {RiRefreshLine} from 'react-icons/ri'
+import {FaThumbsUp} from 'react-icons/fa'
+import {HiOutlineThumbUp} from 'react-icons/hi'
+
 // import { useRef } from 'react';
 // import emailjs from 'emailjs-com';
 
@@ -23,6 +30,27 @@ const Contact:React.FC<Iprop> = ({fontFamily}) => {
   //   e.target.reset();
   // };
 
+  const [message, setMessage] = useState('');
+  const [load, setLoad] = useState(true)
+  const [messageSent, setMessageSent] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
+  const sendEmail = (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault();
+      setMessage('');
+      setMessageSent(true);
+      setButtonDisabled(true);  
+      setLoad(true)
+      setTimeout(() => {
+        setLoad(false);
+      }, 1000);
+      setTimeout(() => {
+          setMessageSent(false);
+          setButtonDisabled(false)
+        }, 2000);
+      
+    };
+
   return (
     <section id='contact' className='px-[62px] max-xlg:px-[20px]'>
         <div className={`${fontFamily} flex items-center gap-8 mb-4  max-sm:flex-col max-sm:items-start max-sm:gap-1`}>
@@ -33,12 +61,26 @@ const Contact:React.FC<Iprop> = ({fontFamily}) => {
       <div
         className='relative gap-6 bg-[#ed240a38] rounded-[20px] p-8'
       >
-        <form  className='flex flex-col gap-6 w-70'>
+        <form onSubmit={(e)=>sendEmail(e as unknown as React.MouseEvent<HTMLButtonElement>)} className='flex flex-col gap-6 w-70'>
           <input type='text' name='name' placeholder='Your Full Name' required className='form-input ' />
           <input type='email' name='email' placeholder='Your email address' required className='form-input ' />
           <input type='text' name='subject' placeholder='Subject' required className='form-input ' />
           <textarea rows={5} name='message' placeholder='Leave a Message' required className='form-input  resize-none' />
-          <button type='submit' className='btn btn-2 btn-4 transition-transform hover:font-bold'>Send Message</button>
+          <button type='submit' 
+          disabled={buttonDisabled}
+          className='btn h-[50px] rounded-[20px] text-white  transition-transform font-semibold'>
+              {messageSent ? (
+                load ? (
+                  <span className='flex justify-center text-[25px] spinner'>
+                    <RiRefreshLine />
+                  </span>
+                ) : (
+                  <span className='text-white flex justify-center items-center gap-1'>Message Sent <span><HiOutlineThumbUp/></span></span>
+                )
+              ) : (
+                'Send Message'
+              )}
+              </button>
         </form>
       </div>
     </section>
