136bc8185de0fc33ce4fb9d4c13fa71cabf635a8
FolashadeOmoloja <omolojashade@gmail.com>
2023-11-23 21:42:06 +0100

    portfolioDetails-contact

--- components/Main.tsx/PortFolioDetails/Work.tsx
+++ components/Main.tsx/PortFolioDetails/Work.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
-const Work = () => {
-  return (
-    <div>Work</div>
-  )
+interface Iprop {
+  fontFamily?: any;
 }
+const Work:React.FC<Iprop> = ({fontFamily}) => {
+  return (
+    <section className="px-[62px] max-xlg:px-[20px]">
+    <div className={`${fontFamily} flex items-center gap-8 mb-4  max-sm:flex-col max-sm:items-start max-sm:gap-1`}>
+        <h2 className='text-[40px] tracking-[2.5%] leading-[40px] font-semibold max-sm:text-[32px] uppercase' >My Porfolio</h2>
+        <div className='bg-btngradient w-[250px] h-[2px] rounded-[20px] max-sm:w-[160px]'></div>
+    </div>
+
+
+    
+</section>
+  );
+};
+
 
 export default Work
