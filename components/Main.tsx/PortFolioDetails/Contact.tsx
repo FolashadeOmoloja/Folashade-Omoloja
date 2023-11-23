4ccf25664997cd42e9cc3722c4f4d08d9087b303
FolashadeOmoloja <omolojashade@gmail.com>
2023-11-23 16:28:25 +0100

    portfolioDetails-contact

--- components/Main.tsx/PortFolioDetails/Contact.tsx
+++ components/Main.tsx/PortFolioDetails/Contact.tsx
@@ -45,22 +45,30 @@ const Contact:React.FC<Iprop> = ({fontFamily}) => {
   };
 
   return (
-    <section id='contact' className='px-[62px] max-xlg:px-[20px]'>
+    <section id='contact' className='px-[62px] max-xlg:px-[20px] flex flex-col justify-between h-full'>
         <div className={`${fontFamily} flex items-center gap-8 mb-4  max-sm:flex-col max-sm:items-start max-sm:gap-1`}>
             <h2 className='text-[40px] tracking-[2.5%] leading-[40px] font-semibold max-sm:text-[32px] uppercase' >Let's talk</h2>
             <div className='bg-btngradient w-[250px] h-[2px] rounded-[20px] max-sm:w-[160px]'></div>
         </div>
+        <div className="flex gap-2 max-lg:my-5">
+        <div className="relative bg-white rounded-lg border border-gray-300 shadow-md p-4 max-w-lg mx-auto">
+         <div className="absolute top-0 -mt-3 w-6 h-6 transform rotate-45 bg-white border border-gray-300"></div>
+         <p className="text-gray-700 text-sm leading-tight">
+          Let's transform your business ideas into captivating online experiences 
+          </p>
+    </div>
+        </div>
 
       <div
-        className='relative gap-6 bg-[#ed240a38] rounded-[20px] p-8 mt-[30px]'
+        className='relative gap-6 bg-[#ed240a38] max-xsm:bg-transparent max-xxsm:px-5 max-xsm:px-0 rounded-[20px] p-8'
       >
-        <img src='girl.svg' alt='' className='absolute top-0 left-0' />
+        <img src='girl.svg' alt='' className='absolute top-[-90px] left-[-10px] w-[75px] h-[146px] max-sm:hidden' />
        
-        <form ref={formRef} onSubmit={(e)=>sendEmail(e)} className='flex flex-col gap-6 w-70'>
+        <form ref={formRef} onSubmit={(e)=>sendEmail(e)} className='flex flex-col gap-6 '>
           <input type='text' name='name' placeholder='Your Full Name' required className='form-input ' />
           <input type='email' name='email' placeholder='Your email address' required className='form-input ' />
           <input type='text' name='subject' placeholder='Subject' required className='form-input ' />
-          <textarea rows={5} name='message' placeholder='Leave a Message' required className='form-input  resize-none' />
+          <textarea rows={4} name='message' placeholder='Leave a Message' required className='form-input  resize-none' />
           <button type='submit' 
           disabled={buttonDisabled}
           className='btn h-[50px] rounded-[20px] text-white  transition-transform font-semibold'>
