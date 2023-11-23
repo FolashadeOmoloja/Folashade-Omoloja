9629f0edec190fbf08890b4540308771094ac879
FolashadeOmoloja <omolojashade@gmail.com>
2023-11-23 23:49:55 +0100

    portfolioDetails-portfolio

--- components/Main.tsx/PortFolioDetails/Work.tsx
+++ components/Main.tsx/PortFolioDetails/Work.tsx
@@ -10,20 +10,20 @@ const Work:React.FC<Iprop> = ({fontFamily}) => {
         <div className='header-line'></div>
     </div>
     <section className='w-full flex justify-center'>
-            <div className='flex gap-8 mt-5 bg-[#F2F5F9] rounded-[50px] px-8 py-4 w-[500px] justify-center max-sm:flex-col max-sm:gap-4 div-center ' >
-                <span className='hover:bg-btngradient'>
+            <div className='flex gap-8 mt-5 bg-[#F2F5F9] rounded-[50px] px-8 py-3 w-[500px] justify-center max-sm:flex-col max-sm:gap-0 div-center ' >
+                <span className='filter-span'>
                   All
                 </span>
-                <span className='hover:bg-btngradient'>
+                <span className='filter-span'>
                   Next js
                 </span>
-                <span className='hover:bg-btngradient'>
+                <span className='filter-span'>
                   React Js
                 </span>
-                <span className='hover:bg-btngradient'>
+                <span className='filter-span'>
                   Javascript
                 </span>
-                <span className='hover:bg-btngradient'>
+                <span className='filter-span'>
                   Design
                 </span>
             </div>
