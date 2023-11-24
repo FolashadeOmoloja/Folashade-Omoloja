cd0b150177a2e82f05943db562660ab8fd3acbce
FolashadeOmoloja <omolojashade@gmail.com>
2023-11-24 23:47:11 +0100

    portfolioDetails-portfolio

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,14 +4,15 @@ import { Pacifico } from 'next/font/google'
 const pacifico = Pacifico({
   subsets: ['latin'],
   weight:['400'],
+  variable: '--font-pacifico'
  
   
 })
 const Header = () => {
   return (
     <header className='flex items-center justify-between my-4'>
-      <div className={`${pacifico.className} text-[24px] max-xxsm:text-[20px] my-3 h-[70px] flex items-center`}>
-         <span>Folashade <span className='bg-text'>Omoloja</span></span> 
+      <div className={` text-[24px] max-xxsm:text-[20px] my-3 h-[70px] flex items-center`}>
+         <span className={`${pacifico.className}`}>Folashade <span className='bg-text'>Omoloja</span></span> 
       </div>
     <ToggleThemeButton/>
     </header>
