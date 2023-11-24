b80c8144089323adb08293f446eb88a1c2d74a91
FolashadeOmoloja <omolojashade@gmail.com>
2023-11-24 23:17:56 +0100

    portfolioDetails-portfolio

--- components/Main.tsx/PortFolioDetails/WorkFilter.tsx
+++ components/Main.tsx/PortFolioDetails/WorkFilter.tsx
@@ -1,25 +1,29 @@
 import React from 'react'
 
-const WorkFilter = () => {
+const items = [
+    'All', 'NextJs', 'ReactJs', 'JavaScript', 'Design'
+]
+
+interface Iprop {
+    fontFamily?: any;
+  }
+const WorkFilter:React.FC<Iprop> = ({fontFamily}) => {
   return (
     <section className='w-full flex justify-center'>
-    <div className='flex gap-8 mt-5 bg-[#F2F5F9] rounded-[50px] px-8 py-3 w-[500px] justify-center max-sm:flex-col max-sm:gap-0 div-center ' >
-        <span className='filter-span'>
-          All
-        </span>
-        <span className='filter-span'>
-          Next js
-        </span>
-        <span className='filter-span'>
-          React Js
-        </span>
-        <span className='filter-span'>
-          Javascript
-        </span>
-        <span className='filter-span'>
-          Design
-        </span>
-    </div>
+    <ul className='flex gap-8 mt-5 bg-[#F2F5F9] rounded-[50px] px-8 py-3 w-[500px] justify-center max-sm:flex-col max-sm:gap-0 div-center ' >
+
+             {
+                  items.map((item,idx)=>{
+                    return (
+                        <li className={`filter-span`} key={idx}>
+                                 {
+                                    item
+                                 }
+                        </li>
+                    )
+                  })
+             }
+    </ul>
 </section>
   )
 }
