77f31271f454e88c4d082b253ca4059bba1a8dd8
FolashadeOmoloja <omolojashade@gmail.com>
2023-12-01 22:55:06 +0100

    changes

--- components/Main.tsx/PortFolioDetails/About.tsx
+++ components/Main.tsx/PortFolioDetails/About.tsx
@@ -48,7 +48,9 @@ const About:React.FC<Iprop> = ({fontFamily}) => {
                        return(
                         <div className="bg-[#ed240a4b] p-4 leading-5 rounded-[20px] lg:min-h-[210px] min-h-[180px]  flex-col div-center qualific-div " key={idx}>
                               <div className="flex gap-5 items-center text-[24px] max-sm:text-xl max-sm:gap-3 max-xxsm:text-base  w-full mb-4 max-sm:mb-2">
-                                <span className={`${item.color}`}>{item.icon}</span>
+                                <span className={`${item.color}`}>
+                                  {item.icon}
+                                  </span>
                                 <h4 className={`font-semibold max-xxsm:font-bold ${fontFamily} uppercase w-full text-start`}>{item.heading}</h4>     
                               </div> 
                               <p className="tracking-[2.5%] text-sm  ">{item.text}</p> 
